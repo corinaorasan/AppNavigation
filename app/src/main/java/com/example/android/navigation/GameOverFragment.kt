69f780fa4f4afdd0fac9873e64bb65a323d3837b
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-02-16 16:18:25 +0200

    Add onClick Handler for tryAgain button

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -29,6 +30,10 @@ class GameOverFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+        binding.tryAgainButton.setOnClickListener() {
+            view?.findNavController()?.navigate(R.id.action_gameOverFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
+
