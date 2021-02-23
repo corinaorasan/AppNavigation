55de01ac76f97e38b8100b4c9655922d21689826
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-02-23 18:21:19 +0200

    Inflate menu resource using menu inflater

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,10 +1,8 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -19,6 +17,11 @@ class TitleFragment : Fragment() {
         setClickListener(binding.playButton)
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
     }
 
     private fun setClickListener(view: View) {
