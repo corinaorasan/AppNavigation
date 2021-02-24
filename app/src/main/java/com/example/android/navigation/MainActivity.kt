ca5a0c60c3b3360cebd7831f7ebd5517c86b77f3
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-02-24 21:10:59 +0200

    Replace Up btn with nav drawer btn on start destination

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -38,6 +38,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
