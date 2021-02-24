280c0f85e1322deef0b91a9ed781ad9ecbea714b
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-02-24 19:10:44 +0200

    Add drawerLayout to setupActionBarWithNavController

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController,drawerLayout)
     }
 
     override fun onSupportNavigateUp(): Boolean {
