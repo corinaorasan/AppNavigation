a3171c144e25a84a275ded541d59ad70bd9b8afd
Orășan Corina <62331686+corinaorasan@users.noreply.github.com>
2021-02-25 15:39:34 +0200

    Add a menu (#3)
    * Modify constraint-layout version
    
    * Finding the controller of NavHostFragment
    
    * Link the NavController to the ActionBar
    
    * Add Override to the onSupportNavigateUp
    
    * Reformat code
    
    * Set fragment title
    
    * Add AboutFragment to the navigation graph
    
    * Add a new menu resource
    
    * Add About menu item
    
    * Tell Android that TitleFragment has a menu
    
    * Inflate menu resource using menu inflater
    
    * Navigate to the About fragment
    
    * Add an item in the menu w/more info about questions
    
    * Rearrange content of About fragment
    
    * Rearrange content of More Info fragment
    
    * Change text alignment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_title.*
@@ -17,7 +16,19 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
         setClickListener(binding.playButton)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+                view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     private fun setClickListener(view: View) {
